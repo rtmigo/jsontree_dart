ad3cf192a8ae37e7ff2d3786026f5205c3afe5ee
Artem IG <ortemeo@gmail.com>
2022-06-30 20:57:08 +0300

    everything is mutable

--- test/int_test.dart
+++ test/int_test.dart
@@ -4,12 +4,22 @@ import 'package:jsontree/jsontree.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('test safe', () async {
+  test('test constructor', () async {
     expect(JsonSafeInt(23).value, 23);
     expect(JsonSafeInt(JsonSafeInt.MAX_SAFE_INTEGER).value, JsonSafeInt.MAX_SAFE_INTEGER);
     expect(JsonSafeInt(JsonSafeInt.MIN_SAFE_INTEGER).value, JsonSafeInt.MIN_SAFE_INTEGER);
     expect(()=>JsonSafeInt(JsonSafeInt.MAX_SAFE_INTEGER+1), throwsArgumentError);
     expect(()=>JsonSafeInt(JsonSafeInt.MIN_SAFE_INTEGER-1), throwsArgumentError);
+  });
+
+  test('test assignment', () async {
+    final jsi = JsonSafeInt(23);
+    expect(jsi.value, 23);
+    jsi.value = 5;
+    expect(jsi.value, 5);
+
+    expect(()=>jsi.value = JsonSafeInt.MAX_SAFE_INTEGER+1, throwsArgumentError);
+    expect(()=>jsi.value = JsonSafeInt.MIN_SAFE_INTEGER-1, throwsArgumentError);
   });
 
 }
